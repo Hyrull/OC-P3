ea03628135d75bb9edf3b09bcd2ec4be74e35039
Hyrull <xavier.leonard@outlook.com>
2024-01-24 19:11:34 +0100

    updates filter's code & added style

--- FrontEnd/photos.js
+++ FrontEnd/photos.js
@@ -35,31 +35,45 @@ async function displayGallery(filter = projets) {
 }
 
 async function displayFilters() {
+    // DOM - Préparation (filtres)
     const sectionPortfolio = document.querySelector("#portfolio")
     const filterDiv = document.createElement("div")
-
+    filterDiv.classList.add('filters')
 
     // Filtre "tous"
-    const tousButton = document.createElement('button')
-    tousButton.innerText = "Tous"
-    tousButton.addEventListener('click', () => {
-        displayGallery()
-    })
-    sectionPortfolio.appendChild(tousButton)
-    
+    const tousButton = createFilterButton("Tous", null)
+    tousButton.classList.add('activefilter')
+    filterDiv.appendChild(tousButton)
+
     // Autres filtres (dynamic)
     for (let i = 0; i < categories.length; i++) {
         const category = categories[i]
+        const filterButton = createFilterButton(category.name, category.id)
+        filterDiv.appendChild(filterButton)
+    }
 
-        const filterButton = document.createElement('button')
-        filterButton.innerText=category.name
+    // Créer un filtre
+    function createFilterButton(name, categoryId) {
+        const button = document.createElement('button')
+        button.innerText = name
 
-        filterButton.addEventListener('click', () => {
-            const filteredProjects = projets.filter(projet => projet.categoryId === category.id)
+        button.addEventListener('click', () => {
+            // Retirer classe "activefilter" des autres boutons
+            const buttons = filterDiv.querySelectorAll('button')
+            buttons.forEach(btn => btn.classList.remove('activefilter'))
+
+            // Ajouter la classe au bouton en question
+            button.classList.add('activefilter')
+
+            // Comparaison de l'id filtre & id catégorie pour l'image (sauf si "tous")
+            const filteredProjects = (name === "Tous")
+                ? projets
+                : projets.filter(projet => projet.categoryId === categoryId)
             displayGallery(filteredProjects)
         })
-        filterDiv.appendChild(filterButton);
+        return button;
     }
+    // DOM - Affichage (filtres)
     const sectionGallery = document.querySelector(".gallery")
     sectionPortfolio.insertBefore(filterDiv, sectionGallery)
 }

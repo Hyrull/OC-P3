7b9bcf2bfa32ca6f740fac287e4baa9f03dc4c82
Hyrull <xavier.leonard@outlook.com>
2024-01-29 23:08:20 +0100

    added modale (empty for now)

--- admintools.js
+++ admintools.js
@@ -42,3 +42,11 @@ if (token) {
 }
 
 // Modale
+const modalContainer = document.querySelector('.modal-container')
+const modalTrigger = document.querySelectorAll('.modal-trigger')
+
+modalTrigger.forEach(trigger => trigger.addEventListener('click', toggleModal))
+
+function toggleModal () {
+  modalContainer.classList.toggle('active')
+}
